89dc20c08abcb32f8cc41d39b91dcbd86c159d78
vinojiniParamasivam <vinojinip76@gmail.com>
2026-07-08 11:57:11 +0530

    feat: add user entity

---
+++ src/main/java/com/vinojinip/ai_knowledge_assistant/auth/entity/User.java
@@ -0,0 +1,30 @@
+package com.vinojinip.ai_knowledge_assistant.auth.entity;
+
+import com.vinojinip.ai_knowledge_assistant.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+}
