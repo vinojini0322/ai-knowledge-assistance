66754e088a8771c074912563264cd400aaa26584
vinojiniParamasivam <vinojinip76@gmail.com>
2026-07-10 13:24:19 +0530

    fix(auth): change login endpoint from GET to POST

--- src/main/java/com/vinojinip/ai_knowledge_assistant/auth/controller/AuthController.java
+++ src/main/java/com/vinojinip/ai_knowledge_assistant/auth/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.vinojinip.ai_knowledge_assistant.auth.dto.LoginResponse;
 import com.vinojinip.ai_knowledge_assistant.auth.dto.RegisterRequest;
 import com.vinojinip.ai_knowledge_assistant.auth.dto.UserResponse;
 import com.vinojinip.ai_knowledge_assistant.auth.entity.User;
-import com.vinojinip.ai_knowledge_assistant.auth.mapper.UserMapper;
 import com.vinojinip.ai_knowledge_assistant.auth.service.AuthService;
 import com.vinojinip.ai_knowledge_assistant.auth.service.UserService;
 import jakarta.validation.Valid;
@@ -31,7 +30,7 @@ public class AuthController {
                 .body(UserMapper.toResponse(savedUser));
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(
             @Valid @RequestBody LoginRequest request) {
 
